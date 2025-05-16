8ba60cfaecec4dd83dba34856bbdbd96a5c24c2f
alima <maktabi876@gmail.com>
2025-05-17 01:21:29 +0330

    fix: modified designs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {};
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export { signInWithPopup };
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
+// export { signInWithPopup };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,15 @@
 import { type Config } from 'tailwindcss';
 
 const config: Config = {
-	content: ['src/**/*.{ts|svelte|html}']
+	content: ['src/**/*.{ts|svelte|html}'],
+	theme: {
+		extend: {
+			colors: {
+				background: '#0D0D1B',
+				foreground: '#ffffff'
+			}
+		}
+	}
 };
 
 export default config;
